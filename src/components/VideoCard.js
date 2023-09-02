f3bebb89c0380caa1609fa38f779ea64ce696170
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-09-02 23:38:26 +0530

    day 2

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -7,7 +7,6 @@ function VideoCard({info }) {
     const {channelTitle, title, thumbnails}=snippet;
     
   return(
-    
     <div className="p-2 m-2 w-60 shadow-lg">
        <img className="rounded-lg" alt="thumbnails" src={thumbnails.medium.url}/>
        <ul>
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -17,7 +17,7 @@ function VideoContainer() {
   return (
     <div className="flex flex-wrap">
       {videos.map((video)=>(
-      <Link to={"/watch?v="+video.id}><VideoCard key={video.id} info={video}/></Link>
+      <Link key={video.id} to={"/watch?v="+video.id}><VideoCard  info={video}/></Link>
       ))}
     </div>
   )
